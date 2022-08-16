47759fa4cb9b20f044e924a68b041ec043fa169e
Hiro527 <hiro527.dev@gmail.com>
2022-08-16 11:34:39 +0900

    [ADD] タスクバーに常駐

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-"""
+'''
 VLaunch (c) 2022 Hiro527
-"""
+'''
 
 import psutil
-import time
 import threading
+from pystray import Icon, Menu, MenuItem
+import time
 import subprocess
-from plyer import notification
+from PIL import Image
 
 VALORANT_PATH = 'C:\Riot Games\VALORANT\live\VALORANT.exe'
 CLAUNCH_PATH = 'C:\Program Files\CLaunch\CLaunch.exe'
 
 CLAUNCH_PID = 0
 
-
-def checkProcess():
+def checkProcess(icon):
     global CLAUNCH_PATH, CLAUNCH_PID, VALORANT_PATH
     VALORANT_DETECTED = False
     CLAUNCH_DETECTED = False
@@ -34,35 +34,39 @@ def checkProcess():
         proc = psutil.Process(CLAUNCH_PID)
         proc.terminate()
         print('[INFO] CLaunch has been terminated')
-        notification.notify(
-            title="自動検知モード",
-            message="CLaunchを停止しました",
-            app_name="VLaunch",
-            timeout=5
-        )
+        icon.notify('CLaunchを停止しました', '自動検知モード')
     if not VALORANT_DETECTED:
         if not CLAUNCH_DETECTED:
             # VALORANT未検出でCLaunchが動いていないときの処理
             subprocess.Popen(r'powershell ./launch.ps1', shell=True)
             print('[INFO] CLaunch Started')
-            notification.notify(
-                title="自動検知モード",
-                message="CLaunchを起動しました。",
-                app_name="VLaunch",
-                timeout=5
-            )
+            icon.notify('CLaunchを起動', '自動検知モード')
+
 
 # 定期実行
-
-
-def setInterval(s, callback):
+def setInterval(icon):
+    icon.visible = True
+    s = 5
     t0 = time.time()
     tN = 0
-    while True:
-        thread = threading.Thread(target=callback)
+    while icon.visible:
+        thread = threading.Thread(target=checkProcess, args=[icon])
         thread.start()
         tN = ((t0 - time.time()) % s) or s
         time.sleep(tN)
 
 
-setInterval(5, checkProcess)
+# 終了
+def quitApp(icon):
+    icon.visible = False
+    TrayIcon.stop()
+
+TrayImage = Image.open('./assets/VLaunch.ico')
+TrayMenu = Menu(
+    MenuItem(
+        '終了',
+        quitApp
+        )
+)
+TrayIcon = Icon(name='VLaunch', icon=TrayImage, title='自動検知モード', menu=TrayMenu)
+TrayIcon.run(setInterval)
